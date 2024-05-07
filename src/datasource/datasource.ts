80f351b12733887a9424d3296e47ef9f1f7116b5
Mykhailo Yevrii <mykhailoyevrii@Mykhailos-MacBook-Pro.local>
2024-05-08 00:44:03 +0300

    fix

--- src/datasource/datasource.ts
+++ src/datasource/datasource.ts
@@ -9,7 +9,7 @@ export const connection = new DataSource({
   port: process.env.DB_PORT ? +process.env.DB_PORT : undefined,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: "typeorm",
+  database: process.env.DB_NAME,
   logging: true,
   entities: [Products, Grocery],
   synchronize: true,
